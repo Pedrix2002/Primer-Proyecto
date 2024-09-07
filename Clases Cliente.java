0dd18ddb279e26ca21c5139059e294ce423b2b04
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-09-06 19:55:53 -0600

    Create Clases Cliente.java

---
+++ Clases Cliente.java
@@ -0,0 +1,21 @@
+public class Cliente {
+    private String nombre;
+    private String direccion;
+
+    public Cliente(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String toString() {
+        return "Cliente: " + nombre + ", Dirección: " + direccion;
+    }
+}
